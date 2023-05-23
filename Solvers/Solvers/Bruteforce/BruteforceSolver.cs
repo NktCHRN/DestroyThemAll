5c693539ee6e25b9712df58e775da6fc3947b6fb
Chernikov Nikita <14nik20@gmail.com>
2023-05-23 19:49:10 +0300

    Refactored Bruteforce solver

--- Solvers/Solvers/Bruteforce/BruteforceSolver.cs
+++ Solvers/Solvers/Bruteforce/BruteforceSolver.cs
@@ -4,41 +4,33 @@ namespace Solvers.Solvers.Bruteforce;
 
 public sealed class BruteforceSolver : ISolver
 {
-    private Solution _currentSolution = null!;
-    private Solution _resultSolution = null!;
-
-    private MilitaryObject[] _militaryObjects = null!;
-    private int _maxSoldiersCount;
-
     public Solution Solve(MilitaryObject[] militaryObjects, int maxSoldiersCount)
     {
-        _currentSolution = new();
-        _resultSolution = new();
-        _militaryObjects = militaryObjects;
-        _maxSoldiersCount = maxSoldiersCount;
+        var currentSolution = new Solution();
+        var resultSolution = new Solution();
 
         SolveInternal(0);
 
-        return _resultSolution;
+        return resultSolution;
 
         void SolveInternal(int startJ)
         {
-            for (var j = startJ; j < _militaryObjects.Length; j++)
+            for (var j = startJ; j < militaryObjects.Length; j++)
             {
-                var currentObject = _militaryObjects[j];
-                if (currentObject.SoldiersCount + _currentSolution.TotalSoldiersCount <= _maxSoldiersCount)
+                var currentObject = militaryObjects[j];
+                if (currentObject.SoldiersCount + currentSolution.TotalSoldiersCount <= maxSoldiersCount)
                 {
-                    _currentSolution.AddLastMilitaryObject(currentObject);
-                    if (_currentSolution.TotalMilitaryObjectsCount > _resultSolution.TotalMilitaryObjectsCount
-                            || (_currentSolution.TotalMilitaryObjectsCount == _resultSolution.TotalMilitaryObjectsCount
-                                && _currentSolution.TotalTime < _resultSolution.TotalTime))
+                    currentSolution.AddLastMilitaryObject(currentObject);
+                    if (currentSolution.TotalMilitaryObjectsCount > resultSolution.TotalMilitaryObjectsCount
+                            || (currentSolution.TotalMilitaryObjectsCount == resultSolution.TotalMilitaryObjectsCount
+                                && currentSolution.TotalTime < resultSolution.TotalTime))
                     {
-                        _resultSolution = (Solution)_currentSolution.Clone();
+                        resultSolution = (Solution)currentSolution.Clone();
                     }
 
                     SolveInternal(j + 1);
 
-                    _currentSolution.RemoveLastMilitaryObject();
+                    currentSolution.RemoveLastMilitaryObject();
                 }
             }
         }
