96749fa1cfe89340b54f329b3a1110e606a7b15d
RuhliadevOleksandr <sasharuhliadev@gmail.com>
2023-05-23 19:14:06 +0300

    Refactored GreedySolver

--- Solvers/Solvers/Greedy/GreedySolver.cs
+++ Solvers/Solvers/Greedy/GreedySolver.cs
@@ -8,45 +8,14 @@ public sealed class GreedySolver : ISolver
     {
         var x = new Solution();
         var sortedObjects = 
-            SortArray((MilitaryObject[])militaryObjects.Clone(), 0, militaryObjects.Length - 1);
-        for (int j = 0; j < sortedObjects.Length; j++)
+            militaryObjects.OrderBy(obj => obj.SoldiersCount / obj.Time);
+        foreach (var obj in sortedObjects)
         {
-            if (maxSoldiersCount-x.TotalSoldiersCount >= sortedObjects[j].SoldiersCount)
+            if (maxSoldiersCount-x.TotalSoldiersCount >= obj.SoldiersCount)
             {
-                x.AddMilitaryObject(sortedObjects[j]);
+                x.AddMilitaryObject(obj);
             }
         }
         return x;
-    }
-    
-    private MilitaryObject[] SortArray(MilitaryObject[] array, int leftIndex, int rightIndex)
-    {
-        var i = leftIndex;
-        var j = rightIndex;
-        var pivot = array[leftIndex].SoldiersCount/array[leftIndex].Time;
-        while (i <= j)
-        {
-            while (array[i].SoldiersCount/array[i].Time < pivot)
-            {
-                i++;
-            }
-            
-            while (array[j].SoldiersCount/array[j].Time > pivot)
-            {
-                j--;
-            }
-            if (i <= j)
-            {
-                (array[i], array[j]) = (array[j], array[i]);
-                i++;
-                j--;
-            }
-        }
-        
-        if (leftIndex < j)
-            SortArray(array, leftIndex, j);
-        if (i < rightIndex)
-            SortArray(array, i, rightIndex);
-        return array;
     }
 }
